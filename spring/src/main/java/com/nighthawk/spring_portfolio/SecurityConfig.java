7cc63f23500513caa7407e5a12ca6f318a0c0038
Tirth-Thakkar <tirth1226@gmail.com>
2024-05-26 15:07:39 -0700

    hopefull cors fix

--- spring/src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
+++ spring/src/main/java/com/nighthawk/spring_portfolio/SecurityConfig.java
@@ -82,9 +82,9 @@ public class SecurityConfig {
 						.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-MaxAge", "600"))
 						.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "POST", "GET",
 								"OPTIONS", "HEAD"))
-				// .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin",
-				// "http://127.0.0.1:4000", "http://localhost:4000", "https://del-norte-farmers-iowa-hawkeyes-fanclub.github.io/IHF-Project-Frontend/"))
-				)
+						.addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin",
+								"http://127.0.0.1:4000", "http://localhost:4000",
+								"https://del-norte-farmers-iowa-hawkeyes-fanclub.github.io/IHF-Project-Frontend/")))
 				.formLogin(form -> form
 						.loginPage("/login"))
 				.logout(logout -> logout
