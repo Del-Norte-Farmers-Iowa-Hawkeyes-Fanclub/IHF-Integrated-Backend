b099c2181f9c0397b1c490fb8ab39045632e0748
VishnuAravind12 <vishnuaa77@gmail.com>
2024-05-27 19:01:10 -0700

    historical corn futures data

---
+++ flask/commodities/histdata.py
@@ -0,0 +1,19 @@
+import requests
+import pandas as pd
+
+def fetch_historical_data():
+    url = "http://data.nasdaq.com/api/v3/datasets/TFGRAIN/CORN.json"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()['dataset']
+        df = pd.DataFrame(data['data'], columns=data['column_names'])
+        df['Date'] = pd.to_datetime(df['Date'])
+        df = df.sort_values(by='Date')
+        return df
+    else:
+        raise Exception(f"Failed to fetch data: {response.status_code}")
+
+# Fetch data
+data = fetch_historical_data()
+print(data.head())
