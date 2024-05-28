82b8cd421c9b81c762ba402839ebfcc5559b97b9
rohinsood <rs.rohinsood@gmail.com>
2024-05-28 01:42:18 -0700

    fix relative pathing

--- flask/api/graph_gen.py
+++ flask/api/graph_gen.py
@@ -6,11 +6,11 @@ import pandas as pd
 from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 from datetime import datetime, timedelta
-from histdata import fetch_historical_data
+from .histdata import fetch_historical_data
 
 def plot_corn_futures(num_historical_points, num_predicted_points, filename):
     # Load the LSTM model
-    model = load_model('flask/commodities/corn_futures_lstm_model.h5')
+    model = load_model('api/corn_futures_lstm_model.h5')
 
     # Fetch the required number of historical data points
     historical_data = fetch_historical_data()[-num_historical_points:]
@@ -82,7 +82,7 @@ def plot_corn_futures(num_historical_points, num_predicted_points, filename):
     plt.tight_layout()
 
     # Save plot to file
-    plt.savefig('flask/commodities/images/' + filename)
+    plt.savefig('api/images/' + filename)
     plt.close()
 
 if __name__ == "__main__":
